beaa785ab32008ffda05b8e47a3798a06ff5e1d5
NoFishLikeIan <andreatitton96@gmail.com>
2022-10-21 13:57:31 +0200

    🦉 attempt to fix click error

--- scrape/src/main.ts
+++ scrape/src/main.ts
@@ -1,17 +1,25 @@
-import puppeteer, { Target } from 'puppeteer';
+import puppeteer from 'puppeteer';
 
 // Environment
 import dotenv from 'dotenv';
 dotenv.config({ path: ".env" });
 
 import { env } from 'process';
+import { debug } from 'console';
 
 const { TARGET, PASSWORD, EMAIL } = env;
 
-const CLICK_DELAY = 2_000;
+const CLICK_DELAY = 1_000;
 const TYPE_DELAY = 100;
 
 const justWait = (seconds:number) => new Promise(r => setTimeout(r, seconds * 1000));
+
+const DEBUG_OPTIONS: puppeteer.PuppeteerLaunchOptions = {
+    headless: false,
+    defaultViewport: { width: 1700, height: 800 },
+    devtools: true,
+
+};
 
 (async () => {
     
@@ -24,13 +32,17 @@ const justWait = (seconds:number) => new Promise(r => setTimeout(r, seconds * 10
         const loginUrl = new URL('/mijn/inloggen', TARGET)
         
         const browser = await puppeteer.launch({
-            headless: false
+            headless: false,
+            defaultViewport: { width: 1700, height: 800 },
+
         });
             
         const page = await browser.newPage()
 
         // Login
         await page.goto(loginUrl.toString(), { waitUntil: "networkidle2" })
+
+        await justWait(15)
 
         await page.click(
             "#CybotCookiebotDialogBodyLevelButtonCustomize",
@@ -57,14 +69,18 @@ const justWait = (seconds:number) => new Promise(r => setTimeout(r, seconds * 10
 
         // Access consumption data
         const consumptionUrl = new URL("/mijn/verbruiksoverzicht", TARGET)
+
         
-        const [response] = await Promise.all([
-            page.goto(consumptionUrl.toString(), { waitUntil: 'networkidle2' }),
-            justWait(10),
-            page.select(".h-full.w-full.rounded.py-2.pl-3.pr-14", "days"),
-            // page.waitForResponse(response => response.url())
+        await Promise.all([
+            page.goto(consumptionUrl.toString()),
+            page.waitForNetworkIdle({ idleTime: 10 }),
         ])
 
+        await page.select(
+            ".h-full.w-full.rounded.py-2.pl-3.pr-14", "days"
+        ).catch(error => {
+            debugger;
+        })
 
         // Done
         await browser.close()
